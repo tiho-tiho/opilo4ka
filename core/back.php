f7957a12920b47ae6f26f5beb35ab9e5288c3634
tiho-tiho <abalakina19@gmail.com>
2020-09-09 14:02:19 +0300

    Финальная версия: убрана форма , поправлено меню, карточки, шрифты. Выгружено на хостинг

--- core/back.php
+++ core/back.php
@@ -5,6 +5,7 @@
 
   $message = "Его зовут: " . $name . ";\nПисать сюда: " . $email . ";\nЗвонить туда: " . $phone . ".";
 
-  mail("abalakina19@gmail.com", "Ура! У вас новый лид с опилочки!", $message);
+  mail("isafronova1303@gmail.com", "Ура! У вас новый лид с опилочки!", $message);
 
   echo json_encode($message);
+    header('Location: https://poizone.ru');
